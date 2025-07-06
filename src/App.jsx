b0ca1aacca77c3d37fd33ed6726d16baf8fb8791
Mehjabin Hossain <evanobab@gmail.com>
2025-07-06 16:02:41 +0600

    Fix routing and base for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/my-portfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
