1a46e0e8af38867c7fb07fe99c10ceb0c33da27a
Mehjabin Hossain <157262679+mehjabinhossain@users.noreply.github.com>
2025-07-06 15:53:43 +0600

    Update vite.config.js
    u

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/, 
+  base: '/my-portfolio/', // ✅ now valid
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
