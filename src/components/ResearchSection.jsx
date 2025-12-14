a48bc89bbd708ab474190f7f42ace38607ee4ab2
Mehjabin Hossain <evanobab@gmail.com>
2025-12-14 17:12:35 +0600

    updated with new papers.

--- src/components/ResearchSection.jsx
+++ src/components/ResearchSection.jsx
@@ -18,6 +18,24 @@ export const ResearchSection = () => {
       image: "./projects/2.png",
       link: "https://link.springer.com/book/9789819683529",
     },
+    {
+      title:
+        "A Multisectoral Study of Mpox Epidemiology, Resistance Surveillance, and Policy Gaps: Toward a One Health Framework",
+      publisher: "Springer Nature journal",
+      description:
+        "Officially accepted for publication in Clinical Reviews in Allergy & Immunology.    A Q1 Springer Nature journal with an Impact Factor of 11.3 and a CiteScore of 18. The paper has been accepted and is currently in the online publication process.",
+      image: "./projects/mpox.jpg",
+      //link: "https://ieeexplore.ieee.org/document/10966494",
+    },
+    {
+      title:
+        "Dual-Head Deep Learning Framework for Automated Skin Disease Detection and Cancer Classification with Explainable AI Support",
+      publisher: "IEEE ICCIT",
+      description:
+        "Officially accepted for publication in Clinical Reviews in Allergy & Immunology.    A Q1 Springer Nature journal with an Impact Factor of 11.3 and a CiteScore of 18. The paper has been accepted and is currently in the online publication process.",
+      image: "./projects/dual.jpg",
+     // link: "https://ieeexplore.ieee.org/document/10966494",
+    },
   ];
 
   return (
@@ -105,7 +123,7 @@ export const ResearchSection = () => {
                   <h3 className="text-2xl font-bold text-white leading-snug">
                     {paper.title}
                   </h3>
-                  <p className="text-sm font-semibold text-pink-400">
+                  <p className="text-xl font-semibold text-pink-400">
                     {paper.publisher}
                   </p>
                   <p className="text-sm text-muted-foreground leading-relaxed">
